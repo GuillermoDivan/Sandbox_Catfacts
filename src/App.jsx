287758baacc05edf7541aa21af17919fe8ad2145
GuillermoDivan <guillermodivan@hotmail.com>
2024-05-09 11:11:55 -0300

    getRandomFact done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import './App.css';
+import { getRandomFact } from "./services/getRandomFact";
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 const CAT_PREFIX_IMAGE_URL = 'http://cataas.com/cat/says/';
 
 export function App(){
@@ -9,13 +9,8 @@ export function App(){
 const [fact, setFact] = useState("");
 const [imageUrl, setImageUrl] = useState("");
 
-useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-    .then(response => response.json())
-    .then(data => {
-        const {fact} = data;
-        setFact(fact);
-})
+
+useEffect(() => {getRandomFact().then(setFact)
 }, [])
 
 useEffect(() => {
@@ -30,10 +25,14 @@ useEffect(() => {
 
 }, [fact])
 
-
+const handleClick= async() => {
+    const newFact = await getRandomFact();
+    setFact(newFact);
+}
     return(
         <main>
             <h1>Catfacts</h1>
+                <button onClick={handleClick}>Get new fact!</button>
                 <section>
                     {fact && <p>{fact}</p>}
                 </section>
